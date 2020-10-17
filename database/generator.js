8832d9013969c5b9ee1074495942e038ec43a2ac
Anna <ann.kazannik@gmail.com>
2020-10-17 10:09:05 -0700

    add comments

--- database/generator.js
+++ database/generator.js
@@ -6,15 +6,22 @@ const faker = require('faker');
 const argv = require('yargs').argv;
 
 const lines = argv.lines || 1000000;
-const filename = argv.output || 'users.csv';
-const stream = fs.createWriteStream(filename);
+const filenameUsers = argv.output || 'users.csv';
+const filenameRooms = argv.output || 'rooms.csv';
+const streamUsers = fs.createWriteStream(filenameUsers);
 
+// ==========================DATA GENERATION ========================
 const createUsers = () => {
   const userName = `${faker.name.firstName()} ${faker.name.lastName()}`;
   return `${userName}\n`;
 };
 
-const startWriting = (writeStream, encoding, done) => {
+const createRooms = () => {
+
+};
+
+// ========================= STREAM WRITING =========================
+const startWritingUsers = (writeStream, encoding, done) => {
   let i = 0;
   const writing = () => {
     const canWrite = true;
@@ -26,21 +33,29 @@ const startWriting = (writeStream, encoding, done) => {
         console.log(`created ${i}-entries`);
       }
       const user = createUsers();
+      // check if i === lines so we would write and call done
       if (i === lines) {
+        // we are done so fire callback
         writeStream.write(user, encoding, done);
         console.timeEnd(`${i}-entries`);
       } else {
+        // not done, keep writing
         writeStream.write(user, encoding);
       }
+      // else call write and continue looping
     } while (i < lines && canWrite);
     if (i < lines && !canWrite) {
+      // our buffer for streamUsers filled and need to wait for drain
+      // Write some more once it drains.
       writeStream.once('drain', writing);
     }
   };
   writing();
 };
 
-stream.write(`name\n`, 'utf-8');
-startWriting(stream, 'utf-8', () => {
-  stream.end();
-});
+// write out header line before invoking the loop
+// streamUsers.write(`name\n`, 'utf-8');
+// invoke startWritingUsers and pass callback
+// startWritingUsers(streamUsers, 'utf-8', () => {
+//   streamUsers.end();
+// });
